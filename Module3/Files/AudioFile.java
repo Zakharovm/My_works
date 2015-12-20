1b2a865d07c2857b5f6634278d9125051bb53342
Max <xpro100bestx@gmail.com>
2015-12-20 20:31:04 +0200

    Update AudioFile.java

--- Module3/Files/AudioFile.java
+++ Module3/Files/AudioFile.java
@@ -1,11 +1,4 @@
 // наследуем класс File аудиофайлом
 class AudioFile extends File {
-    private String bitrate;
-
-    // создаем и конструктор
-    public AudioFile(String n, String s, String p) {
-        name = n;
-        extension = s;
-        bitrate = p;
-    }
+    
 }
