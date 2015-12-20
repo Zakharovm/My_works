c9d35670f61b2a432de4c0cf203511b94b7e2ad8
Max <xpro100bestx@gmail.com>
2015-12-20 20:39:28 +0200

    Update Directory.java

--- Module3/Files/Directory.java
+++ Module3/Files/Directory.java
@@ -1,3 +1,5 @@
 public class Directory {
-
+    File image = new File("C:/folder1/image.jpg");
+    File audio = new File("C:/folder1/image.jpg");
+    File video = new File("C:/folder1/image.jpg");
 }
