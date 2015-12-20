f1e7dc4c5dda9129360eb12809d648a4e59a49d3
Max <xpro100bestx@gmail.com>
2015-12-20 20:32:06 +0200

    Update ImageFile.java

--- Module3/Files/ImageFile.java
+++ Module3/Files/ImageFile.java
@@ -1,11 +1,4 @@
 // наследуем класс File графическим файлом
 class ImageFile extends File {
-    private String resolution;
 
-    // создаем и конструктор
-    public ImageFile(String n, String s, String p) {
-        name = n;
-        extension = s;
-        resolution = p;
-    }
 }
