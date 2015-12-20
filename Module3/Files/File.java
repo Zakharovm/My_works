94eea5a4daf6439d2bd9ec4c2aef8975c64e800c
Max <xpro100bestx@gmail.com>
2015-12-20 20:31:43 +0200

    Update File.java

--- Module3/Files/File.java
+++ Module3/Files/File.java
@@ -1,10 +1,5 @@
 class File {
 
-             Directory image = new Directory("C:/folder1/image.jpg");
-             Directory audio = new Directory("C:/folder1/hello.mp3");
-             Directory video = new Directory("C:/folder1/prestige.avi");
-            protected String name;
-            protected String extension;
         }
 
 
