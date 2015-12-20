8980907e8389415183b576459797b5e880aa6999
Max <xpro100bestx@gmail.com>
2015-12-20 20:32:19 +0200

    Update TextFile.java

--- Module3/Files/TextFile.java
+++ Module3/Files/TextFile.java
@@ -1,12 +1,4 @@
 // наследуем класс File текстовым файлом
 class TextFile extends File {
-    private String size;
-
-    // создаем и конструктор
-    public TextFile(String n, String s, String p) {
-        name = n;
-        extension = s;
-        size = p;
-    }
 
 }
