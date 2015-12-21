4d778ea6240f242cedc583608fa8667fb12771d9
Max <xpro100bestx@gmail.com>
2015-12-21 22:43:16 +0200

    Initial commit

--- Module4/Square/src/FindSquare.java
+++ Module4/Square/src/FindSquare.java
@@ -6,9 +6,9 @@ public class FindSquare {
     Circle circle = new Circle(15);
 
 
-        System.out.println("The square of triangle is: " + new Calculation().findTriangleSquare(triangle));
-        System.out.println("The square of rectangle is: " + new Calculation().findRectangleSquare(rectangle));
-        System.out.println("The square of circle is: " + new Calculation().findCircleSquare(circle));
+        System.out.println("The square of triangle is: " + new Calculators().findTriangleSquare(triangle));
+        System.out.println("The square of rectangle is: " + new Calculators().findRectangleSquare(rectangle));
+        System.out.println("The square of circle is: " + new Calculators().findCircleSquare(circle));
     }
 
 }
