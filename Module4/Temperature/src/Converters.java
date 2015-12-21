d0fe714c01eb6ddacfd2ad84bf6c26a2387cbb0d
Max <xpro100bestx@gmail.com>
2015-12-21 22:53:29 +0200

    Initial commit

--- Module4/Temperature/src/Converters.java
+++ Module4/Temperature/src/Converters.java
@@ -1,12 +1,12 @@
 public class Converters {
 
     // Result = ((9*x)/5)+32  из Цельсия в Фаренгейты
-    public double celsiusToFahrenheit(float Celsius) {
-        return 32+((9*Celsius)/5);
+    public double celsiusToFahrenheit(float celsius) {
+        return 32+((9*celsius)/5);
     }
 
     // Result = (x-32)*5/9  из Фаренгейтов в Цельсии
-    public double fahrenheitToCelsius(float Fahrenheit) {
-        return (Fahrenheit-32)*5/9;
+    public double fahrenheitToCelsius(float fahrenheit) {
+        return (fahrenheit-32)*5/9;
     }
 }
