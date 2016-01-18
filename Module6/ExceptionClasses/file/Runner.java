e4ee1775df89580349b77c71629c56d80d405ac5
Max <xpro100bestx@gmail.com>
2016-01-18 22:41:38 +0200

    Filename checking moved to the class File

--- Module6/ExceptionClasses/file/Runner.java
+++ Module6/ExceptionClasses/file/Runner.java
@@ -6,19 +6,13 @@ import java.util.Scanner;
 public class Runner {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        final File[] filesArray = {new AudioFile("123.mp3"), new TextFile("data.txt"), new ImageFile("image.jpg")};
+        final File[] filesArray = {new AudioFile(""), new TextFile("data.txt"), new ImageFile("image.jpg")};
+        Directory directory = new Directory("Файлы", filesArray);
 
         System.out.println("Input the name of the file, which you want to find: ");
         String fileName = scanner.nextLine();
         try {
-            if (fileName == null || fileName.equals("")) {
-                throw new WrongFileNameException(fileName);
-            } else {
-                System.out.println("Your file is " + new Directory("Файлы", filesArray).getFile(fileName).getName());
-            }
-
-        } catch (WrongFileNameException e) {
-            System.out.println("[Error!]: Your file name is ' " + e.getFileName() + "' , but it should consist of at least one letter or number");
+            System.out.println("Your file is " + directory.getFile(fileName).getName());
         } catch (FileNotFoundException e) {
             System.out.print("файл " + fileName + " не найден");
         }
