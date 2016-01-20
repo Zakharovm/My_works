8af6b2cab0a3eb593af3f008993adbd5b6114cf6
Max <xpro100bestx@gmail.com>
2016-01-20 16:58:14 +0200

    test

--- Module1/src/FP1.java
+++
@@ -1,5 +0,0 @@
-public class FP1 {
-    public static void main(String[] args){
-        System.out.println("Hello, World! Welcome to JavaCore.");
-    }
-}
--- Module6/ExceptionClasses/flower/Rose.java
+++
@@ -1,6 +0,0 @@
-package ExceptionClasses.flower;
-
-// наследуем класс Flower классом роза
-public class Rose extends Flower {
-
-}
