e2a57f4618295a1e6e2ff8154f952f94f3ddc940
Max <xpro100bestx@gmail.com>
2016-01-20 17:28:17 +0200

    Added module 7

---
+++ Module7/src/instrument/instrument/Piano.java
@@ -0,0 +1,16 @@
+package ExceptionClasses.instrument;
+
+// наследуем класс Instrument классом пианино
+public class Piano extends Instrument {
+
+    public Piano(String name, int price) {
+        super(name,price);
+    }
+
+    public Piano (String name) {
+        super(name);
+    }
+    public Piano (int price) {
+        super(price);
+    }
+}
