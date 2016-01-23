360e37f66f883806c53a180d729ed2020b10c35d
Max <xpro100bestx@gmail.com>
2016-01-23 17:24:55 +0200

    Added runner

---
+++ Module8/src/collections/file/Runner.java
@@ -0,0 +1,21 @@
+package collections.file;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args) {
+        Directory directory = new Directory("Files", 220, new ArrayList<>(Arrays.asList(new Audio("a.mp3", 10), new Image("picture.jpg", 5), new Text("hello.txt", 1), new Image("image.jpg", 4), new Text("photo.txt", 200))));
+        FileUtils fileUtils = new FileUtils();
+
+        System.out.println("Basic list: ");
+        fileUtils.print(directory.getArrayList());
+        directory.sortByName(directory.getArrayList());
+        System.out.println("\nThe list, sorted by name: ");
+        fileUtils.print(directory.getArrayList());
+        directory.sortBySize(directory.getArrayList());
+        System.out.println("\nThe list, sorted by size: ");
+        fileUtils.print(directory.getArrayList());
+
+    }
+}
