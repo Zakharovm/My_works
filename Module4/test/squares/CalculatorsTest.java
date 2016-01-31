89cd39f91e78dbece72a6a57b487f0893eede197
Max <xpro100bestx@gmail.com>
2016-01-31 18:20:24 +0200

    Added tests;

--- Module4/test/squares/CalculatorsTest.java
+++ Module4/test/squares/CalculatorsTest.java
@@ -1,33 +1,47 @@
 package squares;
 
-import static org.junit.Assert;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 
 public class CalculatorsTest {
-    private Calculators calculators;
+    private static Calculators calculators;
 
-    @Before
-    public void setUp () throws Exception {
+
+
+    @BeforeClass
+    public static void setUpClass () {
         calculators = new Calculators();
     }
 
     @Test
-    public void testFindTriangleSquare() throws Exception {
-        double side1 = 2;
-        double side2 = 2;
-        double side3 = 2;
-        double result = ca;
-        return Assert.assertEquals(sqrt(3), result);
+    public void testFindTriangleSquare() {
+        final double side1 = 2;
+        final double side2 = 2;
+        final double side3 = 2;
+        final Triangle triangle = new Triangle(side1, side2, side3);
+        final double result = calculators.findTriangleSquare(triangle);
+
+        Assert.assertEquals(Math.sqrt(3) ,result, 0.0001);
     }
 
     @Test
-    public void testFindRectangleSquare() throws Exception {
+    public void testFindRectangleSquare() {
+        final double side1 = 2;
+        final double side2 = 4;
+        final Rectangle rectangle = new Rectangle(side1, side2);
+        final double result = calculators.findRectangleSquare(rectangle);
 
+        Assert.assertEquals(side1*side2, result, 0.0001);
     }
 
     @Test
-    public void testFindCircleSquare() throws Exception {
+    public void testFindCircleSquare() {
+        final double radius = 10;
+        final Circle circle = new Circle(radius);
+        final double result = calculators.findCircleSquare(circle);
 
+        Assert.assertEquals(Math.PI*radius*radius, result, 0.0001);
     }
 }
