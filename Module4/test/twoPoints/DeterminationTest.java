10ebe9f02de97e9e246818f5f80613a7d496f78a
Max <xpro100bestx@gmail.com>
2016-02-01 13:09:07 +0200

    Added tests;

--- Module4/test/twoPoints/DeterminationTest.java
+++ Module4/test/twoPoints/DeterminationTest.java
@@ -1,12 +1,24 @@
 package twoPoints;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class DeterminationTest {
+    private Determination determination;
+
+    @BeforeClass
+    public void setUpClass() {
+        determination = new Determination();
+
+    }
 
     @Test
-    public void testCalculateDistance() throws Exception {
+    public void testCalculateDistance() {
+        final Point point1 = new Point(1,1);
+        final Point point2 = new Point(1,5);
+        final double result = determination.calculateDistance(point1, point2);
 
+        Assert.assertEquals(4, result, 0.001);
     }
 }
