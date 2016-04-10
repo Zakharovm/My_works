0aeb84a84886a389109b06da76c30177ed1447d2
Max <xpro100bestx@gmail.com>
2016-04-10 19:54:48 +0300

    fixed names of exception classes

--- EnterpriseCourse/Module 2/src/main/java/Generics/ExecutorImpl.java
+++ EnterpriseCourse/Module 2/src/main/java/Generics/ExecutorImpl.java
@@ -1,6 +1,10 @@
 package generics;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class ExecutorImpl implements Executor<Long> {
 
